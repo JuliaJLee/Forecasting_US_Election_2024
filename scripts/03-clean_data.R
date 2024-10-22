0abc686a6f90d672ad0449ff03bf8d8bc933358c
Julia Lee <jlee.lee@mail.utoronto.ca>
2024-10-22 02:57:59 -0400

    Finished an initial data cleaning script and saved the cleaned data as "analysis_data" in the analysis data folder.

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,62 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Clean the raw Presidential General Election Polls (current cycle) data 
+# Author: Tianning He, Julia Lee, and Shuangyuan Yang
+# Date: 22 October 2024 
+# Contact: *need to fill-in
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Download and save the actual Presidential General Election Polls 
+# (current cycle) data from 538
+# Any other information needed? N/A
 
-#### Workspace setup ####
+
+#### Setting Up the Workspace ####
+
 library(tidyverse)
+library(dplyr)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+#### Cleaning the Data ####
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
+# Read in the raw data
+
+data <- read_csv("data/01-raw_data/raw_data.csv")
+
+# Create a new dataframe with desired variables
+# Want to keep the pollster name, numeric grades, state, the date when the data was 
+# created, the population of voters, whether the poll considered a hypothetical 
+# match-up, the candidate name, and the percentage of vote
+
+colnames(data)
+
+columns_to_keep <- c("pollster","numeric_grade", "state", "population", "created_at", 
+                     "hypothetical", "candidate_name", "pct")
+
+new_data = data[columns_to_keep]
+#view(new_data)
+
+# Now want to filter on high-quality pollsters (>= 2.7), likely voters (lv),
+# non-hypothetical match-ups (hypothetical = False), and Harris
+
+filtered_data <- new_data |>
+  filter(
+    numeric_grade >= 2.7,
+    population == "lv",
+    hypothetical == "FALSE",
+    candidate_name == "Kamala Harris")
+
+#view(filtered_data)
+
+# Change the NA values in the "state" column to "National"
+
+cleaned_data <- filtered_data |>
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+    state = if_else(is.na(state), "National", state))
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+view(cleaned_data)
+
+
+#### Saving the Data ####
+
+# Save the cleaned data as a csv file in data/02-analysis_data
+# The csv file will be called "analysis_data.csv"
+
+write_csv(cleaned_data, file = "data/02-analysis_data/analysis_data.csv")
