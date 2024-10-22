3e56d0fc8d92c2e8725176395a098100a75ebfc2
Julia Lee <jlee.lee@mail.utoronto.ca>
2024-10-22 02:03:42 -0400

    Deleted unnecessary files (i.e. literature folder and the python simulation script). Also, completed simulation script and saved simulated data. Need to go back and add contact information (emails) to the script later.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,52 +1,67 @@
 #### Preamble ####
-# Purpose: Simulates a dataset of Australian electoral divisions, including the 
-  #state and party that won each division.
-# Author: Rohan Alexander
-# Date: 26 September 2024
-# Contact: rohan.alexander@utoronto.ca
+# Purpose: Simulate the Presidential General Election Polls (current cycle) Data
+# Author: Tianning He, Julia Lee, and Shuangyuan Yang
+# Date: 22 October 2024 
+# Contact: *need to fill-in
 # License: MIT
-# Pre-requisites: The `tidyverse` package must be installed
-# Any other information needed? Make sure you are in the `starter_folder` rproj
+# Pre-requisites: Review the plan and sketches for the analysis in the "other" folder
+# Any other information needed? N/A
 
 
-#### Workspace setup ####
+#### Setting Up the Workspace ####
 library(tidyverse)
-set.seed(853)
 
+#### Simulating the Data ####
 
-#### Simulate data ####
-# State names
-states <- c(
-  "New South Wales",
-  "Victoria",
-  "Queensland",
-  "South Australia",
-  "Western Australia",
-  "Tasmania",
-  "Northern Territory",
-  "Australian Capital Territory"
-)
+set.seed(420)
 
-# Political parties
-parties <- c("Labor", "Liberal", "Greens", "National", "Other")
+# Want to generate a table that shows high-quality pollsters with a numeric grade greater than or 
+# equal to 2.7 and whether each pollster was national or state-specific. 
+# Want the table to also outline the date in which the poll data was created and the percentage of 
+# votes or support for the candidate, Harris.
 
-# Create a dataset by randomly assigning states and parties to divisions
-analysis_data <- tibble(
-  division = paste("Division", 1:151),  # Add "Division" to make it a character
-  state = sample(
-    states,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.25, 0.25, 0.15, 0.1, 0.1, 0.1, 0.025, 0.025) # Rough state population distribution
-  ),
-  party = sample(
-    parties,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.40, 0.40, 0.05, 0.1, 0.05) # Rough party distribution
-  )
-)
+# Define table parameters (i.e. the columns of the table)
 
+high_quality_pollsters = c("Suffolk", "AtlasIntel", "SurveyUSA", "Siena", "Marquette Law School", 
+                           "Beacon/Shaw")
 
-#### Save data ####
-write_csv(analysis_data, "data/00-simulated_data/simulated_data.csv")
+pollster_numeric_grade = c(2.7, 2.8, 2.9, 3)
+
+national_or_state = c("National", "Pennsylvania", "Minnesota", "Wisconsin", "Arizona", "Nevada",
+                      "Georgia", "Michigan")
+
+candidate = c("Kamala Harris")
+
+## Setting start and end dates to simulate the date in which the poll data was created
+
+start_date = as.Date("2024-07-26")
+end_date = as.Date("2024-10-21")
+
+## Setting the number of observations (i.e. polls) to simulate
+
+num_obs = 50
+
+## Getting the percentage of votes for Harris
+
+pct_votes = round(runif(num_obs, 40, 60), 1)
+
+# Generate a random sample of high-quality pollsters 
+
+simulated_data <- 
+  tibble(
+    pollsters = sample(high_quality_pollsters, num_obs, replace = TRUE),
+    numeric_grade = sample(pollster_numeric_grade, num_obs, replace = TRUE),
+    scope_of_poll = sample(national_or_state, num_obs, replace = TRUE),
+    data_created = sample(seq(from = start_date, to = end_date, by = "day"), 
+                          num_obs, replace = TRUE),
+    candidate_name = sample(candidate, num_obs, replace = TRUE),
+    percentage_of_vote = sample(pct_votes, num_obs))
+
+#view(simulated_data)
+
+#### Saving the Data ####
+
+# Save the simulated data as a csv file in data/00-simulated_data
+# The csv file will be called "simulated_data.csv"
+
+write_csv(simulated_data, "data/00-simulated_data/simulated_data.csv")
