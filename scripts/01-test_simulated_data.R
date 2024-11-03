ffcf47e2f1061a7d2d3712817c8a8541f923aab6
Julia Lee <jlee.lee@mail.utoronto.ca>
2024-11-03 15:15:15 -0500

    Completed code for tests 4, 5, and 6 + updated test 1 to include a test for pollster names.

--- scripts/01-test_simulated_data.R
+++ scripts/01-test_simulated_data.R
@@ -27,8 +27,8 @@ is.na(data)
 
 data < 0
 
-# (2) Test for valid state names, populations, candidate names, and hypothetical 
-# match-ups
+# (2) Test for valid state names, populations, candidate names, hypothetical 
+# match-ups, and pollster names
 
 test_that("There are valid state names", {
   expect_true(all(data$state %in% c("National", "Pennsylvania", "Minnesota", "Wisconsin", 
@@ -43,6 +43,10 @@ test_that("There are valid candidate names", {
 test_that("Hypothetical match-up values are either false or true", {
   expect_true(all(data$hypothetical_match_up %in% c("FALSE", "TRUE")))})
 
+test_that("There are valid pollster names", {
+  expect_true(all(data$pollsters %in% c("Suffolk", "AtlasIntel", "SurveyUSA", "Siena", "Marquette Law School", 
+                                        "Beacon/Shaw")))})
+
 # (3) Test that pct does not exceed 100 (i.e. is 
 # between 0 and 100)
 
@@ -53,13 +57,18 @@ test_that("pct for both candidates is between 0 and 100", {
 # (4) Test that end date comes after start date
 
 test_that("The end date of a poll is a date after its start date", {
-  expect_true(all(data$end_date 0 & data$pct <= 100))
+  expect_true(all(data$end_date > data$start_date))
 })
 
-# (5) Test that sample size is appropriate (i.e. greater than 30)
+# (5) Test that sample size is appropriate (i.e. greater than or 
+# equal to 30)
 
-
+test_that("Sample sizes are greater than or equal to 30", {
+  expect_true(all(data$sample_size >= 30))
+})
 
 # (6) Test for numeric grade (i.e. that it is between 2.7 and 3)
 
-
+test_that("Pollster numeric grades are between 2.7 and 3, including endpoints", 
+          {expect_true(all(data$numeric_grade >= 2.7 & data$numeric_grade <=3))
+})
