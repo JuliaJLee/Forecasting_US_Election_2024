853666214176a54c34552b094556aa80e8b07cb2
Julia Lee <jlee.lee@mail.utoronto.ca>
2024-11-03 15:52:55 -0500

    Completed data cleaning script, saved the cleaned analysis data as a parquet file in the analysis data folder, and removed the previous analysis data that was saved as a csv file.

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Clean the raw Presidential General Election Polls (current cycle) data 
-# Author: Tianning He, Julia Lee, and Shuangyuan Yang
+# Author: Julia Lee
 # Date: 22 October 2024 
-# Contact: *need to fill-in
+# Contact: jlee.lee@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Download and save the actual Presidential General Election Polls 
 # (current cycle) data from 538
@@ -13,6 +13,7 @@
 
 library(tidyverse)
 library(dplyr)
+library(arrow)
 
 #### Cleaning the Data ####
 
@@ -20,43 +21,44 @@ library(dplyr)
 
 data <- read_csv("data/01-raw_data/raw_data.csv")
 
-# Create a new dataframe with desired variables
-# Want to keep the pollster name, numeric grades, state, the date when the data was 
-# created, the population of voters, whether the poll considered a hypothetical 
-# match-up, the candidate name, and the percentage of vote
+# Want to clean the data so that the data:
+# (1) is filtered on pollsters with a numeric grade of 2.7 and higher
+# (2) shows state-specific polls
+# (3) shows polls that targeted the likely voter population
+# (4) displays the start and end dates of polls between July 7, 2024 and October 26, 2024
+# (5) presents the sample size of each poll
+# (6) displays polls that did not ask about hypothetical match-ups between candidates
+# (7) is filtered on Harris and Trump
+# (8) shows the pct for Harris and Trump
+ 
+## Selecting columns that correspond with the variables listed above
 
 colnames(data)
 
-columns_to_keep <- c("pollster","numeric_grade", "state", "population", "created_at", 
-                     "hypothetical", "candidate_name", "pct")
+columns_to_keep <- c("pollster","numeric_grade", "state", "start_date", "end_date", "sample_size",
+                     "population", "hypothetical", "candidate_name", "pct")
 
 new_data = data[columns_to_keep]
+
 #view(new_data)
 
-# Now want to filter on high-quality pollsters (>= 2.7), likely voters (lv),
-# non-hypothetical match-ups (hypothetical = False), and Harris
+## Filtering the selected columns: high-quality pollsters (>= 2.7), state-specific polls 
+## (i.e. not national ones), likely voters (lv), non-hypothetical match-ups 
+## (hypothetical = False), and Harris and Trump
 
 filtered_data <- new_data |>
   filter(
     numeric_grade >= 2.7,
+    state != is.na(state),
     population == "lv",
     hypothetical == "FALSE",
-    candidate_name == "Kamala Harris")
+    candidate_name == "Kamala Harris" | candidate_name == "Donald Trump")
 
 #view(filtered_data)
 
-# Change the NA values in the "state" column to "National"
-
-cleaned_data <- filtered_data |>
-  mutate(
-    state = if_else(is.na(state), "National", state))
-
-#view(cleaned_data)
-
-
 #### Saving the Data ####
 
-# Save the cleaned data as a csv file in data/02-analysis_data
-# The csv file will be called "analysis_data.csv"
+# Save the cleaned data as a parquet file in data/02-analysis_data
+# The file will be called "analysis_data.parquet"
 
-write_csv(cleaned_data, file = "data/02-analysis_data/analysis_data.csv")
+write_parquet(filtered_data, "data/02-analysis_data/analysis_data.parquet")
